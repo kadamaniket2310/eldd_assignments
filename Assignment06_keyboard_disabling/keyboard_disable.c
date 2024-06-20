b1c89ced1940cde9cc19dafddd4c569226e3f2c5
E1_84112_Aniket <aniketkadam319@gmail.com>
2024-06-20 14:26:00 +0530

    assign

--- Assignment06_keyboard_disabling/keyboard_disable.c
+++ Assignment06_keyboard_disabling/keyboard_disable.c
@@ -1,12 +1,13 @@
 #include <linux/module.h>
-#include <linux/timer.h>
+#include <linux/kernel.h>
 #include <linux/interrupt.h>
-#include <linux/delay.h>
+#include <linux/timer.h>
 #include <linux/io.h>
+//#include <sys/io.h>
 
-#define KBD_DATA_REG 0x60
-#define KBD_CONTROL_REG 0x64
-#define DELAY do { mdelay(1); if (++delay > 10) break; } while(0)
+
+#define KBD_DATA_REG        0x60    /* I/O port for keyboard data */
+#define KBD_CONTROL_REG        0x64
 
 struct timer_list mytimer;
 int ticks;
@@ -14,33 +15,24 @@ int count = 0;
 
 void mytimer_function(struct timer_list *ptimer)
 {
-	long delay =0;
-	printk(KERN_INFO " %s : mytimer_function : count = %d\n", THIS_MODULE->name, count);
+	//printk(KERN_INFO " %s : mytimer_function : count = %d\n", THIS_MODULE->name, count);
 	count++;
-	printk(KERN_INFO " %s : Reenabling keyboard starting...\n", THIS_MODULE->name);
-	while(inb(KBD_CONTROL_REG) & 0x02)
-		DELAY;
-	outb(0xAE, KBD_CONTROL_REG);
-	printk(KERN_INFO " %s : Renabling keyboard completed...\n", THIS_MODULE->name);
+    outb(0xAE, KBD_CONTROL_REG); //enable keyboard
+	//mod_timer(&mytimer, jiffies + ticks);
 }
 
 static __init int desd_init(void)
 {
-	long delay =0;
 	int sec = 10;
-	ticks = sec * HZ;		//2500
+	ticks = sec * HZ;		//750
 
 	timer_setup(&mytimer, mytimer_function, 0);
 	mytimer.expires = jiffies + ticks;
 	add_timer(&mytimer);
-	printk(KERN_INFO " %s : Disabling keyboard starting...\n", THIS_MODULE->name);
-	while(inb(KBD_CONTROL_REG) & 0x02)
-		DELAY;
-	outb(0xAD, KBD_CONTROL_REG);
-	printk(KERN_INFO " %s : Disabling keyboard completed...\n", THIS_MODULE->name);
-	
+    outb(0xAD, KBD_CONTROL_REG); //disbale keyboar
+    
 
-	printk(KERN_INFO " %s : Timer initialisation is done successfully\n", THIS_MODULE->name);
+	//printk(KERN_INFO " %s : Timer initialisation is done successfully\n", THIS_MODULE->name);
 	return 0;
 }
 
@@ -54,5 +46,18 @@ module_init(desd_init);
 module_exit(desd_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Aniket");
-MODULE_DESCRIPTION("This is keyboard disabling demo");
+MODULE_AUTHOR("DESD @ Sunbeam");
+MODULE_DESCRIPTION("This is demo of kernel timers");
+
+
+
+
+
+
+
+
+
+
+
+
+
